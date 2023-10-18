c37114ddb50236de90db9a3f9bbe80eda15aa677
Thanadon Mahaparn <thanadon.mhp@gmail.com>
2023-10-18 23:28:45 +0700

    New Containers system. w/ routimg and empty blade.php for the future.

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Container;
+use App\Models\Journey;
 use Illuminate\Http\Request;
 
 class ContainerController extends Controller
 {
-    //
+    public function index()
+    {
+        $containers = Container::get();
+        return view('containers.index' , [
+            'containers' => $containers
+        ]);
+    }
+
+    public function create()
+    {
+        return view('containers.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'company_name' => ['required', 'string', 'min:1']
+        ]);
+        $journey = Journey::find($request->get('journey'));
+
+        $container = new Container();
+        $container->journey_id = $journey->id;
+        $container->company_name = $request->get('company_name');
+        $container->status = 'PENDING';
+
+        $container->save();
+
+        $containers = Container::get();
+        return view('containers.index' , [
+            'containers' => $containers
+        ]);
+    }
+
+    public function update(Request $request, Container $container, Journey $journey)
+    {
+        $request->validate([
+            'status' => ['required', 'string', 'min:1']
+        ]);
+
+        $container->status = $request->get('status');
+
+        $container->save();
+
+        $containers = Container::get();
+        return view('containers.index' , [
+            'containers' => $containers
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContainerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/containers', [ContainerController::class, 'index'])->name('containers.index');
+    Route::get('/containers/create', [ContainerController::class, 'create'])->name('containers.create');
+    Route::post('/containers/update/{journey}/{container}', [ContainerController::class, 'update'])->name('containers.update');
 });
 
 require __DIR__.'/auth.php';
